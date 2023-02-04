14ae49fd1444ecbdcace225a0544ba9115059a0a
ShevtsovAlexander <95102126+ShevtsovAlexander@users.noreply.github.com>
2023-02-04 14:19:15 +0300

    Add files via upload

---
+++ Multiply.js
@@ -0,0 +1,55 @@
+const option1 = document.querySelector('#option1')
+const option2 = document.querySelector('#option2')
+const option3 = document.querySelector('#option3')
+
+const audio = document.querySelector('#myAudio')
+
+var answer = 0;
+
+function generate_equation() {
+    var num1 = Math.floor(Math.random() * 13)
+    var num2 = Math.floor(Math.random() * 13)
+
+    var dummyAnswer1 = Math.floor(Math.random() * 10)
+    var dummyAnswer2 = Math.floor(Math.random() * 10)
+
+    var allAnswers = [];
+    var switchAnswers = [];
+
+    answer = eval(num1 * num2);
+
+    document.querySelector('#num1').innerHTML = num1;
+    document.querySelector('#num2').innerHTML = num2;
+
+    var allAnswers = [answer, dummyAnswer1, dummyAnswer2];
+
+    for (i = allAnswers.length; i--;){
+        switchAnswers.push(allAnswers.splice(Math.floor(Math.random() * (i + 1)), 1)[0]);
+      };
+    
+      option1.innerHTML = switchAnswers[0];
+      option2.innerHTML = switchAnswers[1];
+      option3.innerHTML = switchAnswers[2]; 
+}
+option1.addEventListener('click', () => {
+    if(option1.innerHTML == answer) {
+        generate_equation();
+    } else {
+        audio.play();
+    }
+})
+option2.addEventListener('click', () => {
+    if(option2.innerHTML == answer) {
+        generate_equation();
+    } else {
+        audio.play();
+    }
+})
+option3.addEventListener('click', () => {
+    if(option3.innerHTML == answer) {
+        generate_equation();
+    } else {
+        audio.play();
+    }
+})
+generate_equation()
